af526b5d28a9ff310d5e55bc7cf5df71e5906c9b
Juan Camilo Blanco Martinez <camilo3198@gmail.com>
2023-05-30 17:18:32 -0500

    add seed

---
+++ src/seed/seed.service.ts
@@ -0,0 +1,34 @@
+import { Get, Injectable } from '@nestjs/common';
+import { ProductsService } from 'src/products/products.service';
+import { initialData } from './data/seed-data';
+
+@Injectable()
+export class SeedService {
+
+  constructor(
+    private readonly productService: ProductsService
+  ){}
+
+  async runSeed(){
+
+    await this.insertNewProducts();
+
+    return 'SEED EXECUTED';
+  }
+
+  private async insertNewProducts(){
+    await this.productService.deleteAllProduct();
+
+    const products = initialData.products;
+    const insertPromises = [];
+
+    products.forEach(e => {
+      insertPromises.push(this.productService.create(e));
+    });
+
+    const results = await Promise.all(insertPromises);
+
+    return true;
+  }
+
+}
